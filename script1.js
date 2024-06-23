9ac79df26b7fd9882decd1093377142db8f68171
david <tochukwud750@gmail.com>
2024-06-23 08:09:23 +0100

    updated a content

--- script1.js
+++ script1.js
@@ -1,15 +1,10 @@
-const optionMenu = document.querySelector(".select-menu"),
-       selectBtn = optionMenu.querySelector(".select-btn"),
-       options = optionMenu.querySelectorAll(".option"),
-       sBtn_text = optionMenu.querySelector(".sBtn-text");
+var tooltip = document.querySelector('.tooltip')
 
-selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));       
-
-options.forEach(option =>{
-    option.addEventListener("click", ()=>{
-        let selectedOption = option.querySelector(".option-text").innerText;
-        sBtn_text.innerText = selectedOption;
-
-        optionMenu.classList.remove("active");
-    });
+tooltip.addEventListener('click', function() {
+  if (this.classList.contains('active')) {
+    this.classList.remove('active');
+  } else {
+    this.classList.add('active');
+  }
+  
 });
