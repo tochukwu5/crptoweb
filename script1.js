ecb4aad27ef76804f50f1504470b5f17fb7c4600
david <tochukwud750@gmail.com>
2024-06-29 14:48:09 +0100

    added a toggler

--- script1.js
+++ script1.js
@@ -1,10 +1,17 @@
-var tooltip = document.querySelector('.tooltip')
 
-tooltip.addEventListener('click', function() {
-  if (this.classList.contains('active')) {
-    this.classList.remove('active');
-  } else {
-    this.classList.add('active');
+//for index.html dark-theme toggle
+
+const toggle = document.getElementById('switch');
+let isSwitched = false
+
+toggle.onclick = function(){
+  isSwitched = !isSwitched;
+  if (isSwitched = isSwitched) {
+    var bg_toggle = document.getElementById("bg-toggle");
+    bg_toggle.innerHTML = "";
+      }
+      else{
+    var bg_toggle = document.getElementById("bg-toggle");
+bg_toggle.innerHTML = "<link rel=" + "stylesheet" + " href=" + "./styletoggle.css" + ">";
   }
-  
-});
+}
---
+++ script2.js
@@ -0,0 +1,17 @@
+//for buypage.html dark-theme toggle
+
+const toggler = document.getElementById('switcherid');
+let isSwitcher = false
+
+toggler.onclick = function(){
+  console.log('1123')
+  isSwitcher = !isSwitcher;
+  if (isSwitcher = isSwitcher) {
+    var bg_toggle = document.getElementById("bg-toggler");
+    bg_toggle.innerHTML = "";
+      }
+      else{
+    var bg_toggle = document.getElementById("bg-toggler");
+bg_toggle.innerHTML = "<link rel=" + "stylesheet" + " href=" + "./styletogglebuypage.css" + ">";
+  }
+}
